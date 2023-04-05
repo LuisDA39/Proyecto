4a36b46bc713cbe3a966530c47b9e5fff0a09253
Luis De Anda <luisdeanda@MacBook-Air-de-Luis.local>
2023-04-05 12:56:00 -0600

    Hola

--- main.cpp
+++ main.cpp
@@ -1,4 +1,66 @@
 #include <iostream>
+using namespace  std;
+
+class tacosComidos {
+private:
+    int tacos[];
+public:
+    void generarArreglo();
+    void imprimirArreglo();
+    void bubbleSort();
+    void selectionSort();
+    void insertionSort();
+    void countingSort();
+    void shellSort();
+    void quickSort();
+    void heapSort();
+    void mergeSort();
+    void radixSort();
+};
+
+void tacosComidos::generarArreglo() {
+
+}
+
+void tacosComidos::imprimirArreglo() {
+
+}
+
+void tacosComidos::bubbleSort() {
+
+}
+
+void tacosComidos::selectionSort() {
+
+}
+
+void tacosComidos::insertionSort() {
+
+}
+
+void tacosComidos::countingSort() {
+
+}
+
+void tacosComidos::shellSort() {
+
+}
+
+void tacosComidos::quickSort() {
+
+}
+
+void tacosComidos::heapSort() {
+
+}
+
+void tacosComidos::mergeSort() {
+
+}
+
+void tacosComidos::radixSort() {
+
+}
 
 int main() {
 

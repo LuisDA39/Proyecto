c54d1fce5298b640dba83ad7658a5e562b52bcf3
LuisDA39 <107770749+LuisDA39@users.noreply.github.com>
2023-04-06 16:40:35 -0600

    Sorts implementados

--- main.cpp
+++ main.cpp
@@ -25,6 +25,85 @@ int partition(int arr[], int start, int end) {
 	return pivotIndex;
 }
 
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int l = 2 * i + 1; 
+    int r = 2 * i + 2; 
+
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void merge(int arr[], int p, int q, int r) {
+    int n1 = q - p + 1;
+    int n2 = r - q;
+
+    int L[n1], M[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[p + i];
+    for (int j = 0; j < n2; j++)
+        M[j] = arr[q + 1 + j];
+
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = p;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= M[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = M[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = M[j];
+        j++;
+        k++;
+    }
+}
+
+int getMax(int arr[], int n){
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)  mx = arr[i];
+    return mx;
+}
+
+void CountSort(int arr[], int n, int exp){
+    int output[n];
+    int i, count[10] = {};
+    for (i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+    for (i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+
 class comeTacos {
 public:
     int noDatos = 365;
@@ -41,7 +120,7 @@ public:
     void shellSort();
     void quickSort(int arr[], int start, int end);
     void heapSort();
-    void mergeSort();
+    void mergeSort(int arr[], int l, int r);
     void radixSort();
 
 };
@@ -50,7 +129,7 @@ void comeTacos::generarArreglo() {
     srand(time(nullptr));
 
     for (int i = 0; i < noDatos; i++) {
-            tacos[i] = (rand() % 10 + 1);
+            tacos[i] = (rand() % 11);
     }
 }
 
@@ -160,15 +239,33 @@ void comeTacos::quickSort(int arr[], int start, int end) {
 
 
 void comeTacos::heapSort() {
+    for (int i = noDatos / 2 - 1; i >= 0; i--) {
+        heapify(tacos, noDatos, i);
+    }
 
+    for (int i = noDatos - 1; i >= 0; i--) {
+        swap(tacos[0], tacos[i]);
+        heapify(tacos, i, 0);
+    }
 }
 
-void comeTacos::mergeSort() {
+void comeTacos::mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
 
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
 }
 
 void comeTacos::radixSort() {
+    int m = getMax(tacos, noDatos); 
 
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        CountSort(tacos, noDatos, exp);
+    
 }
 
 int main() {
